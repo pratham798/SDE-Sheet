c1324829d94dc836cbb285d2352631d54f743dd2
Pratham Jamnal <prathamjamnal@Prathams-MacBook-Air.local>
2023-03-04 21:08:49 +0530

    Aggressive Cows

---
+++ AggressiveCows.cpp
@@ -0,0 +1,33 @@
+// Time Complexity: O(NlogN)+O(NlogM)=>O(NlogM)
+// Space Complexity: O(1)
+#include <bits/stdc++.h> 
+bool isPossible(vector<int>&positions,int gap,int c)
+{
+	int count=1,prev=0;
+	for(int i=1;i<positions.size();i++)
+	{
+		if(abs(positions[i]-positions[prev])>=gap)
+		{
+			count++;
+			prev=i;
+		}
+		if(count==c) return true;
+	}
+	return false;
+}
+int chessTournament(vector<int> positions , int n ,  int c){
+	// Write your code here
+	sort(positions.begin(),positions.end());
+	int start=0,end=positions[n-1],res=0;
+	while(start<=end)
+	{
+		int mid=(start+end)/2;
+		if(isPossible(positions,mid,c))
+		{
+			res=mid;
+			start=mid+1;
+		}
+		else end=mid-1;
+	}
+	return res;
+}
