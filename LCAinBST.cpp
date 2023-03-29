853c4414dd9f7966b4dccdf4b82a055e15ba0552
Pratham Jamnal <prathamjamnal7982@gmail.com>
2023-03-29 14:24:41 +0530

    Lowest Common Ancestor In A BST

---
+++ LCAinBST.cpp
@@ -0,0 +1,12 @@
+//Time Complexity: O(H)
+//Space Complextiy: O(1)
+
+Node* LCA(Node *root, int n1, int n2)
+{
+   //Your code here
+   if(!root) return root;
+   if(root->data==n1||root->data==n2) return root;
+   if(root->data>n1 and root->data>n2) return LCA(root->left,n1,n2);
+   if(root->data<n1 and root->data<n2) return LCA(root->right,n1,n2);
+   return root;
+}
